fa39f8d22e4251d7c0068511744beabfb20615db
zota <zota@example.com>
2025-04-16 23:32:14 +0900

    fix: sugoi overload

--- proto1.py
+++ proto1.py
@@ -18,31 +18,52 @@ class Ones(Tile):
 class Basket:
     def __init__(self):
         self.value = 0
-        self.prev_value = 0
+        self.tens_bin = []
+        self.ones_bin = []
+    
+    @classmethod
+    def from_value(cls, value):
+        instance = cls()
+        instance.put(value)
+        return instance
 
-    def set_value(self):
+    def put(self,value):
+        b10 = value // 10
+        b1 = value % 10
+        self.tens_bin = [Tens()] * b10
+        self.ones_bin = [Ones()] * b1
+
+    def __str__(self):
+        return f'{Tens()} : {len(self.tens_bin)}\n{Ones()} : {len(self.ones_bin)}'
+
+    def __add__(self):
+        return self.b10
+
+
+class UI:
+    def __init__(self):
+        self.value = 0
+        pass
+    
+    def input(self):
         try:
             self.value = int(input())
         except ValueError:
             self.value = 0
+        return self.value
 
-        diff = self.value - self.prev_value
-        print(f'{self.value} - {self.prev_value} = {diff}')
-        self.prev_value = self.value
-
-#        b10 = amount // 10
-#        b1 = amount % 10
-#        print(f'{b10}, {b1}')
 
 if __name__ == '__main__':
-    tens = Tens()
-    print(tens)
-
+    
+    ui = UI()
     bsk = Basket()
+    
+    bsk2 = Basket.from_value(14)
+    print(bsk2)
+
     while True:
-        bsk.set_value()
+        v = ui.input()
+        bsk.put(v)
+        print(bsk)
 
 
-#    bsk = Basket()
-#    bsk.add(54)
-#    print(bsk)
